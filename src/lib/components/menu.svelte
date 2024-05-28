89166e91df57f4c41e3d5a07348ee510b28138ff
Duneya <54691201+Duneyasaleh@users.noreply.github.com>
2024-05-28 15:31:06 +0200

    eerste versie van navigatie voor mobiel en desktop
    In deze commit staat de eerste versie van de navigatie voor zowel mobiel als desktop.
    
    Ik heb icoontjes toegevoegd aan een iconenbibliotheek, waardoor ze netjes kunnen worden ingeladen.
    
    Daarnaast heb ik de code gerefactored.

--- src/lib/components/menu.svelte
+++ src/lib/components/menu.svelte
@@ -1,52 +1,127 @@
 <script>
-</script>
+	import { PhoneIcon, HamburgerMenu } from '$lib/index.js';
+	let navActive = false;
+  
+	const toggleNav = () => {
+	  navActive = !navActive;
+	};
+  </script>
+  
+  <header>
+	<a href="/">
+	  <img src="assets/NadineBeautyLogo.svg" alt="Nadine beauty" width="200" height="200" class="logo"/>
+	</a>
+	<a href="javascript:void(0)" class="menu-icon" on:click={toggleNav}>
+	  <img src={HamburgerMenu} alt="Hamburger Menu" class="hamburgermenu"/>
+	</a>
+	<nav class:active={navActive}>
+	  <ul role="menu">
+		<li><a href="/">Home</a></li>
+		<li><a href="#aboutMe">About me</a></li>
+		<li><a href="#gallery">Gallery</a></li>
+		<li><a href="tel:0000000"><img src={PhoneIcon} alt="Phone" width="25" height="25"></a></li>
+	  </ul>
+	  <a href="javascript:void(0)" class="close-icon" on:click={toggleNav}>
+		<img src={HamburgerMenu} alt="Close Menu"/>
+	  </a>
+	</nav>
+  </header>
+  
+  <style>
+	header {
+	  position: absolute;
+	  z-index: 99;
+	  width: 100%;
+	  padding: 1em 2.5em 1em 2em;
+	  height: 11vh;
+	  display: flex;
+	  align-items: center;
+	  justify-content: space-between;
+	  background-color: var(--nav-background);
+	}
+	.menu-icon img {
+	  width: 30px;
+	  height: 30px;
+	}
+	nav {
+	  position: fixed;
+	  inset: 0;
+	  background-color: var(--nav-background);
+	  font-size: 1.5em;
+	  display: none;
+	  opacity: 0;
+	  transition: opacity 0.25s ease-in;
+	}
+	nav.active {
+	  display: block;
+	  opacity: 1;
+	}
+	nav .close-icon {
+	  background: none;
+	  padding: 1em 1.5em;
+	  position: absolute;
+	  top: 0;
+	  right: 0;
+	}
+	ul {
+	  list-style: none;
+	  display: flex;
+	  gap: 1em;
+	  flex-direction: column;
+	  padding: 5em 2em;
+	}
+	a {
+	  text-decoration: none;
+	  background: radial-gradient(circle farthest-corner at center center, #ffe28e 0%, #c2980f 24%, #eccb66 50%, #d7b23b 78%, #c2980f 100%);
+		-webkit-background-clip: text;
+		color: transparent;
+		display: inline-block;
+	}
 
-<section>
-	<div class="logo">
-		<img src="assets/NadineBeautyLogo.svg" alt="" />
-	</div>
-	<div class="menu">
-		<ul>
-			<li></li>
-			<li></li>
-			<li></li>
-		</ul>
-	</div>
-</section>
 
-<style>
-	section {
+	@media (min-width: 56.25em) {
+	  .menu-icon, .close-icon {
+		display: none;
+	  }
+	  nav {
 		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		padding: 0 1rem;
-		background-color: var(--nav-background);
+		position: absolute;
+		flex-wrap: wrap;
+		justify-content: flex-end;
+		align-content: center;
+		background: none;
+		padding: 1.5em 2.5em;
+		opacity: 1;
+		transition: none;
+	  }
+	  .logo {
+		position: relative;
+		z-index: 33;
+	  }
+	  ul {
+		flex-direction: row;
+		width: max-content;
+		gap: 2em;
+		padding: 0;
+	  }
+	  a:hover {
+		color: var(--nav-lines);
+	  }
 	}
-
-	img {
-		width: 70%;
+	@media (min-width: 87.5em) {
+	  nav {
+		position: absolute;
+		/* font-size: .em; */
+	  }
+	  nav li{
+		padding: 0;
+	  }
+	  li{
+		border-bottom: .1em solid var(--nav-lines);
 	}
-
-	ul:hover {
-		cursor: pointer;
+	li:last-child {
+    border-bottom: none;
+  }
 	}
-	li {
-		height: 0.3rem;
-		margin: 0.3rem;
-		border-radius: 1rem;
-		list-style: none;
-		background-color: var(--nav-lines);
-	}
-
-	li:nth-child(1) {
-		width: 2.6rem;
-	}
-
-	li:nth-child(2) {
-		width: 3rem;
-	}
-
-	li:nth-child(3) {
-		width: 2.1rem;
-	}
-</style>
+  </style>
+  
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,5 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+//icons
+export {default as PhoneIcon} from '$lib/icons/phone-icon.svg'
+export {default as HamburgerMenu} from '$lib/icons/Hamburger-menu.svg'
