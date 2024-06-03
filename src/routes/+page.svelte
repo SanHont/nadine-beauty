93dbf9056bf4c8cf17c7cc3f331bcd569866ca72
SanHont <dehontsander@gmail.com>
2024-06-03 14:25:36 +0200

    Change in order

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,9 @@
 	import AboutMe from '$lib/components/aboutme.svelte';
 	import HeroHeader from '$lib/components/heroheader.svelte';
 
-
 	export let data;
 </script>
 
 <Menu {data} />
-<AboutMe {data} />
 <HeroHeader {data} />
+<AboutMe {data} />
